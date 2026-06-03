c1e33ae59f726e2339bf84593c7616579ff641e5
Rajyasri_S <rajyasrisrikanth@gmail.com>
2026-06-03 20:35:03 +0530

    Static folder made safe

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,11 @@ from fastapi.staticfiles import StaticFiles
 
 from backend.app.api.routes import router
 from backend.app.api.auth import router as auth_router
+import os
 
 app = FastAPI()
+
+os.makedirs("static", exist_ok=True)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
